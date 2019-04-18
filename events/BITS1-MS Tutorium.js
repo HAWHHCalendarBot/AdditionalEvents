fe29fb98c5ea4b169bf81b9c232f814629a3aae5
m4ny <49698802+m4ny@users.noreply.github.com>
2019-04-18 16:18:22 +0200

    Create BITS1-MS Tutorium.js

---
+++ events/BITS1-MS Tutorium.js
@@ -0,0 +1,92 @@
+[
+  {
+    "year": 2019,
+    "month": 4,
+    "date": 25,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 5,
+    "date": 2,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 5,
+    "date": 9,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 5,
+    "date": 16,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 5,
+    "date": 23,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 5,
+    "date": 30,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 6,
+    "date": 6,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 6,
+    "date": 13,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 6,
+    "date": 20,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  },
+  {
+    "year": 2019,
+    "month": 6,
+    "date": 27,
+    "name": "BITS1-MS Tutorium",
+    "starttime": "09:30",
+    "endtime": "11:30",
+    "room": "BT7 02.65"
+  }
+]
