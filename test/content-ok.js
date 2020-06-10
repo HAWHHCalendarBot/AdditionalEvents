81dd74875a6d236b9c8b553c19877884dd2b1101
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-06-10 22:52:50 +0200

    refactor: update xo and ava

--- test/content-ok.js
+++ test/content-ok.js
@@ -1,5 +1,5 @@
-import {readdirSync, promises} from 'fs'
-import test from 'ava'
+const {readdirSync, promises} = require('fs')
+const test = require('ava')
 
 const {readFile} = promises
 
@@ -9,20 +9,20 @@ for (const filename of readdirSync(DIR)) {
 	test(filename, async t => {
 		const content = JSON.parse(await readFile(`${DIR}/${filename}`))
 		t.log(content)
-		for (const e of content) {
-			checkEvent(t, e)
+		for (const event of content) {
+			checkEvent(t, event)
 		}
 	})
 }
 
-function checkEvent(t, e) {
+function checkEvent(t, event) {
 	// Not needed: room
 
-	t.truthy(e.name)
-	t.truthy(e.starttime)
-	t.truthy(e.endtime)
+	t.truthy(event.name)
+	t.truthy(event.starttime)
+	t.truthy(event.endtime)
 
-	t.true(Number.isFinite(e.date) && e.date >= 1 && e.date <= 31)
-	t.true(Number.isFinite(e.month) && e.month >= 1 && e.month <= 12)
-	t.true(Number.isFinite(e.year) && e.year >= 2015 && e.year <= 2100)
+	t.true(Number.isFinite(event.date) && event.date >= 1 && event.date <= 31)
+	t.true(Number.isFinite(event.month) && event.month >= 1 && event.month <= 12)
+	t.true(Number.isFinite(event.year) && event.year >= 2015 && event.year <= 2100)
 }
