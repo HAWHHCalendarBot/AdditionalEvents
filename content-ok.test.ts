86510e83b7e7d73834471e8d7bfe152835f1f835
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-03-21 10:46:01 +0100

    style: use tabs and add editorconfig

--- content-ok.test.ts
+++ content-ok.test.ts
@@ -3,37 +3,37 @@ import Ajv from "https://esm.sh/ajv@8.11.0";
 const DIR = "./events";
 
 const eventSchema = {
-  type: "object",
-  properties: {
-    name: { type: "string" },
-    room: { type: "string" },
-    starttime: { type: "string", pattern: "^[12]?\\d:\\d\\d" },
-    endtime: { type: "string", pattern: "^[12]?\\d:\\d\\d" },
-    date: { type: "integer", minimum: 1, maximum: 31 },
-    month: { type: "integer", minimum: 1, maximum: 12 },
-    year: { type: "integer", minimum: 2015, maximum: 2100 },
-  },
-  required: ["name", "room", "starttime", "endtime", "date", "month", "year"],
-  additionalProperties: false,
+	type: "object",
+	properties: {
+		name: { type: "string" },
+		room: { type: "string" },
+		starttime: { type: "string", pattern: "^[12]?\\d:\\d\\d" },
+		endtime: { type: "string", pattern: "^[12]?\\d:\\d\\d" },
+		date: { type: "integer", minimum: 1, maximum: 31 },
+		month: { type: "integer", minimum: 1, maximum: 12 },
+		year: { type: "integer", minimum: 2015, maximum: 2100 },
+	},
+	required: ["name", "room", "starttime", "endtime", "date", "month", "year"],
+	additionalProperties: false,
 };
 
 const ajv = new Ajv();
 const validate = ajv.compile(eventSchema);
 
 for (const filename of Deno.readDirSync(DIR)) {
-  Deno.test({
-    name: filename.name,
-    fn() {
-      const content = JSON.parse(
-        Deno.readTextFileSync(`${DIR}/${filename.name}`),
-      ) as unknown[];
-      // console.log(content)
-      for (const event of content) {
-        if (!validate(event)) {
-          console.error(event, validate.errors);
-          throw new Error("does not comply to json schema");
-        }
-      }
-    },
-  });
+	Deno.test({
+		name: filename.name,
+		fn() {
+			const content = JSON.parse(
+				Deno.readTextFileSync(`${DIR}/${filename.name}`),
+			) as unknown[];
+			// console.log(content)
+			for (const event of content) {
+				if (!validate(event)) {
+					console.error(event, validate.errors);
+					throw new Error("does not comply to json schema");
+				}
+			}
+		},
+	});
 }
